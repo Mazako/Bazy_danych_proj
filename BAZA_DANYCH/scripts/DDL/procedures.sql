dfe37ba1956676e90c0b1f51c94bbc2ada7c24d8
michał <michal.maziarz12@gmail.com>
2023-11-25 01:35:46 +0100

    improvements

--- BAZA_DANYCH/scripts/DDL/procedures.sql
+++ BAZA_DANYCH/scripts/DDL/procedures.sql
@@ -10,9 +10,9 @@ $$
         FROM room
         INNER JOIN room_tour ON room.id = room_tour.room_id
         INNER JOIN tour ON room_tour.tour_id = tour.id
-        WHERE search_room_id = room.id
-            AND start_date BETWEEN tour.departure_date AND tour.return_date
-            OR end_date BETWEEN tour.departure_date AND tour.return_date
+        WHERE (search_room_id = room.id)
+            AND (start_date BETWEEN tour.departure_date AND tour.return_date)
+            OR (end_date BETWEEN tour.departure_date AND tour.return_date)
             OR (start_date <= tour.departure_date AND end_date >= tour.return_date);
     RETURN (room_count = 0) IS TRUE;
     END;
