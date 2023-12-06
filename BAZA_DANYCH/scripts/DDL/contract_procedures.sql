f489c9e3e12de4879a116d9d5ab0adbda8faf484
apox <apox2115@gmail.com>
2023-12-06 22:43:09 +0100

    4 new trigers and test_case for them

--- BAZA_DANYCH/scripts/DDL/contract_procedures.sql
+++ BAZA_DANYCH/scripts/DDL/contract_procedures.sql
@@ -59,4 +59,43 @@ BEGIN
     WHERE contract.tour_id = tour.id
     AND CURRENT_DATE - tour.return_date >= 0;
 END;
-$$
+$$;
+
+
+CREATE OR REPLACE FUNCTION update_resort_avg_opinion()
+    RETURNS TRIGGER AS $$
+DECLARE
+    resort_id_to_update int8;
+    new_avg_opinion REAL;
+BEGIN
+    SELECT tour.resort_id INTO resort_id_to_update
+    FROM opinion
+             JOIN contract ON opinion.contract_id = contract.id
+             JOIN tour ON contract.tour_id = tour.id
+    WHERE opinion.id = COALESCE(NEW.id, OLD.id);
+
+    SELECT AVG(o.rate) INTO new_avg_opinion
+    FROM opinion o
+             JOIN contract c ON o.contract_id = c.id
+             JOIN tour t ON c.tour_id = t.id
+    WHERE t.resort_id = resort_id_to_update;
+
+    UPDATE resort
+    SET avg_opinion = new_avg_opinion
+    WHERE id = resort_id_to_update;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE FUNCTION check_reservation_before_departure()
+    RETURNS TRIGGER AS $$
+BEGIN
+    IF (SELECT departure_date FROM tour WHERE id = NEW.tour_id) <= NEW.reservation_date THEN
+        RAISE EXCEPTION 'Data rezerwacji musi być przed datą wyjazdu!';
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
